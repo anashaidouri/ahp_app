bf299024f1a434739f54ad6d41df7e90b96ec1fa
anas haidouri <anas.haidouri11@gmail.com>
2023-05-31 11:42:56 +0100

    test

--- ahp/ahp_app/urls.py
+++ ahp/ahp_app/urls.py
@@ -10,5 +10,3 @@ urlpatterns = [
     path('register/', views.register)
 ]
 
-
-
--- ahp/ahp_app/views.py
+++ ahp/ahp_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+# dfkjslgjslkjgslk
 
 def register(request):
     form = UserCreationForm()
